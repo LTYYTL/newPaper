8be7a963939ccbd9f47e6ec2cbca79086f35aae8
LTYYTL <lzy991362541@163.com>
2019-01-07 14:58:16 +0800

    标题显示完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,16 @@
 //index.js
-
+/*const topTitleMap = {
+  'gn': '国内',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}*/
 
 
 Page({
+  data: {
+    topTitleList: ['国内','国际','财经','娱乐','军事','体育','其他']
+  },
 })
